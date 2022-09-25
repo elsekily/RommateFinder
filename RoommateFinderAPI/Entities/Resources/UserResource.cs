ff9697306e28aac9ae3135961f14d7004aebc447
elsekily <youssefelsekily@outlook.com>
2022-09-25 17:25:48 +0200

    Adding User Resource

---
+++ RoommateFinderAPI/Entities/Resources/UserResource.cs
@@ -0,0 +1,10 @@
+namespace RoommateFinderAPI.Entities.Resources
+{
+    public class UserResource
+    {
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
